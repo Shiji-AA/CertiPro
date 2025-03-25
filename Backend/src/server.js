5e84ef86053dfe419784eb3c06b69ce67c726260
shijiaa <shijihcl@gmail.com>
2025-03-25 16:28:22 +0530

    changes added

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import UserRoutes from "./Routes/UserRoutes/UserRoutes.js";
 import adminRouter from "./Routes/AdminRoutes/AdminRoutes.js";
 import { connectDB } from './config/db.js';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 
 dotenv.config();
@@ -11,14 +13,31 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Define __dirname for ES module compatibility
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
-app.use(cors())
+const corsOptions = {
+    origin: ['http://localhost:4000', 'https://certipro.arcite.in', 'https://www.certipro.arcite.in', 'http://localhost:3000'],
+    methods: "GET, PUT, POST, PATCH, DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"]
+  };
+  app.use(cors(corsOptions));
+
+
 app.use('/api/users', UserRoutes);
 app.use('/api/admin', adminRouter);
 
+// Serving static files
+app.use(express.static(join(__dirname, '../../Frontend/dist')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, "../../Frontend/dist/index.html"));
+});
 
 app.get("/", (req, res) => res.send("Server is ready"));
 
