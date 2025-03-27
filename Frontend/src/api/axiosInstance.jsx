2d5aceeef8dedb8c96eed83b46841824063cad74
shijiaa <shijihcl@gmail.com>
2025-03-27 09:43:01 +0530

    chages added

--- Frontend/src/api/axiosInstance.jsx
+++ Frontend/src/api/axiosInstance.jsx
@@ -9,69 +9,69 @@ const axiosInstanceAdmin = axios.create({
 
 // *********************************************************************************
 // Request interceptor User
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    console.log(token,"interceptor page token")
-    if (token !== null) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     console.log(token,"interceptor page token")
+//     if (token !== null) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
  // Response interceptor
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    if (
-      error.response &&
-      error.response.data.status === 401 &&
-      error.response.data.error === "Unauthorized - No token found"
-    ) {
-      console.log("Unauthorized access - no token found");
-    }
-    return Promise.reject(error);
-  }
-);
+// axiosInstance.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     if (
+//       error.response &&
+//       error.response.data.status === 401 &&
+//       error.response.data.error === "Unauthorized - No token found"
+//     ) {
+//       console.log("Unauthorized access - no token found");
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 // *********************************************************************************
 // Request interceptor Admin
-axiosInstanceAdmin.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("adminToken");
-    console.log(token,"interceptor page Admin token")
-    if (token !== null) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// axiosInstanceAdmin.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("adminToken");
+//     console.log(token,"interceptor page Admin token")
+//     if (token !== null) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
  // Response interceptor
-axiosInstanceAdmin.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    if (
-      error.response &&
-      error.response.data.status === 401 &&
-      error.response.data.error === "Unauthorized - No token found"
-    ) {
-      console.log("Unauthorized access - no token found");
-    }
-    return Promise.reject(error);
-  }
-);
+// axiosInstanceAdmin.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     if (
+//       error.response &&
+//       error.response.data.status === 401 &&
+//       error.response.data.error === "Unauthorized - No token found"
+//     ) {
+//       console.log("Unauthorized access - no token found");
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 
 export { axiosInstance,axiosInstanceAdmin};
